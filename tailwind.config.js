93350d456802b3bb54ee78182d6634aa56e742ab
sayeemzaman3 <sayeem312@gmail.com>
2025-08-17 12:01:15 +0000

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "rich-black": "#050B17",
+        veronica: "#9A4EF6",
+        cornsilk: "#FFF7E0",
+        "mikado-yellow": "#FBC42A",
+        "antiflash-white": "#E8EBF1",
+        "antiflash-white-2": "#EFF0F4",
+      },
+    },
   },
   plugins: [],
 };
