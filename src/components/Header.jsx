56a3ad8bc461ee3fe5c51a2ab466b9686dbfa456
sayeemzaman3 <sayeem312@gmail.com>
2025-08-17 12:08:46 +0000

    add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+// import Nav from "./Nav.jsx";
+
+export default function Header() {
+  return (
+    <header className="w-full h-[80px] p-[16px] bg-rich-black text-white flex justify-between items-center fixed top-0 left-0">
+      <p className="text-white text-xl font-bold">Sayeem</p>
+      {/*<Nav />*/}
+    </header>
+  );
+}
