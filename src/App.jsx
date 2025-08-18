ffcc443d5ef63f4a0c2b6df6292760088d33e55c
sayeemzaman3 <sayeem312@gmail.com>
2025-08-18 15:22:04 +0000

    add Projects sect

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
       <About />
       <Services />
       <Skills />
+      <Projects />
       <Footer />
     </>
   );
---
+++ src/components/Projects.jsx
@@ -0,0 +1,46 @@
+import SectHead from "./SectHead";
+import ProjectCard from "./ProjectCard";
+import ecom from "../assets/ecom-project.png";
+import form from "../assets/form-project.png";
+import preview from "../assets/preview-project.jpg";
+
+const data = [
+  {
+    img: ecom,
+    title: "E-commerce Product Page",
+    desc: "This is a webpage with image slider, cart, responsive layout and many more.",
+    link: "https://e-commerce-product-page-coral-beta.vercel.app/",
+  },
+  {
+    img: form,
+    title: "Multi-Step Form",
+    desc: "This is a form ui with multiple pages, it has personal info & plan selector, breadcrumbs, a final page and many more",
+    link: "https://multi-step-form-theta-ten.vercel.app/",
+  },
+  {
+    img: preview,
+    title: "Product Preview Card",
+    desc: "This is a simple product Preview card made with react.",
+    link: "https://product-preview-card-omega-five.vercel.app/",
+  },
+];
+
+export default function Projects() {
+  return (
+    <div className="sect">
+      <SectHead name="Selected Work" className="self-center" />
+      <p className="prg text-center">These are some of my best projects</p>
+      <div className="flex flex-col gap-[16px]">
+        {data.map((project, index) => (
+          <ProjectCard
+            key={index}
+            img={project.img}
+            title={project.title}
+            desc={project.desc}
+            link={project.link}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
