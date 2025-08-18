d06b2cbe52b2a672ea6208992c7af350f6af8aa3
sayeemzaman3 <sayeem312@gmail.com>
2025-08-18 13:13:18 +0000

    add Skills sect

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
+import Skills from "./components/Skills";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Hero />
       <About />
       <Services />
+      <Skills />
     </>
   );
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,41 @@
+import SectHead from "./SectHead";
+import SkillCard from "./SkillCard";
+import html from "../assets/html-5.png";
+import css from "../assets/css-3.png";
+import js from "../assets/js.png";
+import jquery from "../assets/jquery.png";
+import git from "../assets/git.png";
+import sass from "../assets/sass.png";
+import react from "../assets/react.png";
+import tailwind from "../assets/tailwind.png";
+
+const lvl = ["Novice", "Beginner", "Proficient", "Advanced", "Expert"];
+const data = [
+  { name: "HTML", icon: html, level: lvl[2] },
+  { name: "CSS", icon: css, level: lvl[2] },
+  { name: "JavaScript", icon: js, level: lvl[3] },
+  { name: "JQuery", icon: jquery, level: lvl[1] },
+  { name: "Git", icon: git, level: lvl[2] },
+  { name: "Sass", icon: sass, level: lvl[1] },
+  { name: "React", icon: react, level: lvl[2] },
+  { name: "Tailwind CSS", icon: tailwind, level: lvl[1] },
+];
+
+export default function Skills() {
+  return (
+    <div className="sect">
+      <SectHead name="Skills" className="self-center" />
+      <p className="prg text-center">I can bring your design into reality</p>
+      <div className=" grid grid-cols-2 gap-[16px]">
+        {data.map((skill, index) => (
+          <SkillCard
+            key={index}
+            name={skill.name}
+            icon={skill.icon}
+            level={skill.level}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
