72f6ebacf8452e6137e44c40edd2310cac0c4ab2
sayeemzaman3 <sayeem312@gmail.com>
2025-08-20 11:24:41 +0000

    add Form validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
 import { ShowForm } from "../contexts/ShowForm";
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import crossIcon from "../assets/cross.png";
 
 function CloseBtn() {
@@ -15,11 +15,43 @@ function CloseBtn() {
   );
 }
 
+function Error({ error }) {
+  return <p className="text-red-500 text-xs">{error}</p>;
+}
+
 export default function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [msg, setMsg] = useState("");
+  const [errors, setErrors] = useState({});
+
+  function hideErrors() {
+    setErrors({});
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newErrors = {};
+
+    if (!name) newErrors.name = "Name is required";
+    if (!email) newErrors.email = "Email is required";
+    else if (!/\S+@\S+\.\S+/.test(email)) newErrors.email = "Email is invalid";
+    if (!subject) newErrors.subject = "Subject is required";
+    if (!msg) newErrors.msg = "Message is required";
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      setErrors({});
+    }
+  }
+
   return (
     <div className="fixed z-20 top-0 left-0 w-dvw h-dvh bg-rich-black/95 py-[32px] px-[16px] flex items-center">
       <form
-        novalidate
+        onSubmit={handleSubmit}
+        noValidate
         action=""
         className="w-full bg-white rounded-[16px] p-[32px] flex flex-col gap-[16px]"
       >
@@ -27,32 +59,53 @@ export default function Form() {
         <p className="font-bold text-xl">
           Tell Me about yourself and what you want to make.
         </p>
-        <input
-          type="text"
-          name="name"
-          placeholder="Full Name"
-          aria-label="Full Name"
-          required
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          aria-label="Email"
-          required
-        />
-        <input
-          type="text"
-          name="subject"
-          placeholder="Subject"
-          aria-label="Subject"
-          required
-        />
-        <textarea
-          name="message"
-          placeholder="Message..."
-          aria-label="Message"
-        ></textarea>
+        <div>
+          <input
+            type="text"
+            name="name"
+            placeholder="Full Name"
+            aria-label="Full Name"
+            onClick={hideErrors}
+            onChange={(e) => setName(e.target.value)}
+          />
+          {errors.name && <Error error={errors.name} />}
+        </div>
+
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            aria-label="Email"
+            onClick={hideErrors}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {errors.email && <Error error={errors.email} />}
+        </div>
+
+        <div>
+          <input
+            type="text"
+            name="subject"
+            placeholder="Subject"
+            aria-label="Subject"
+            onClick={hideErrors}
+            onChange={(e) => setSubject(e.target.value)}
+          />
+          {errors.subject && <Error error={errors.subject} />}
+        </div>
+
+        <div>
+          <textarea
+            name="message"
+            placeholder="Message..."
+            aria-label="Message"
+            onClick={hideErrors}
+            onChange={(e) => setMsg(e.target.value)}
+          ></textarea>
+          {errors.msg && <Error error={errors.msg} />}
+        </div>
+
         <button
           type="submit"
           name="submit"
