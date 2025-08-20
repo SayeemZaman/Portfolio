b742ebd2acd76fc06864577037bde8feb5a317df
sayeemzaman3 <sayeem312@gmail.com>
2025-08-20 08:52:24 +0000

    improve centering div

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -2,7 +2,7 @@ export default function SkillCard({ icon, name, level }) {
   return (
     <div className="rounded-full flex flex-col gap-[4px] justify-center items-center p-[16px] border border-antiflash-white w-full aspect-square">
       <img className="w-[32px]" src={icon} alt={`${icon} Icon`} />
-      <div className="flex flex-col justify-center items-center">
+      <div className="center">
         <p className="font-bold">{name}</p>
         <p className="font-medium font-xs text-antiflash-white">{level}</p>
       </div>
