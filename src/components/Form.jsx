93948bdd49da83c25dde86d4588d5420d541fe1d
sayeemzaman3 <sayeem312@gmail.com>
2025-08-20 08:52:59 +0000

    add CloseBtn to Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,20 @@
+import { ShowForm } from "../contexts/ShowForm";
+import { useContext } from "react";
+import crossIcon from "../assets/cross.png";
+
+function CloseBtn() {
+  const { toggleForm } = useContext(ShowForm);
+
+  return (
+    <button
+      onClick={toggleForm}
+      className="rounded bg-veronica/20 center w-[32px] h-[32px] ml-auto"
+    >
+      <img src={crossIcon} alt="Close Button" className="w-[12px] h-[12px]" />
+    </button>
+  );
+}
+
 export default function Form() {
   return (
     <div className="fixed z-20 top-0 left-0 w-dvw h-dvh bg-rich-black/95 py-[32px] px-[16px] flex items-center">
@@ -6,6 +23,7 @@ export default function Form() {
         action=""
         className="w-full bg-white rounded-[16px] p-[32px] flex flex-col gap-[16px]"
       >
+        <CloseBtn />
         <p className="font-bold text-xl">
           Tell Me about yourself and what you want to make.
         </p>
