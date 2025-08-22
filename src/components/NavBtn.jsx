9c36f3eac530c207fca8317febfb801fef98cc3b
sayeemzaman3 <sayeem312@gmail.com>
2025-08-22 04:54:28 +0000

    add NavBtn

---
+++ src/components/NavBtn.jsx
@@ -0,0 +1,9 @@
+import menu from "../assets/menu-burger.png";
+
+export default function NavBtn({ isActive, setIsActive }) {
+  return (
+    <button className="w-[32px]" onClick={() => setIsActive(!isActive)}>
+      <img src={menu} alt="Nav Button" className="w-[24px]" />
+    </button>
+  );
+}
